8378d5a086288849f84f731e74ed3f2b359b4124
Jamie Barlow <jamiebarlow@gmail.com>
2025-08-29 15:43:06 +0100

    Setup Prettier for Tailwind

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 export const newUserPage = () => {
-  return (
-    <div>New User Page</div>
-  )
-}
+  return <div>New User Page</div>;
+};
 
-export default newUserPage
+export default newUserPage;
