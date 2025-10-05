c287bb091d3f542cee00ef0f9a0c7e5078d4bc1b
Jamie Barlow <jamiebarlow@gmail.com>
2025-10-05 17:51:46 +0100

    Add sitemap and robots.txt generation on build with next-sitemap

---
+++ next-sitemap.config.cjs
@@ -0,0 +1,5 @@
+// next-sitemap.config.js
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://jfwoxfordcounselling.co.uk", // Your website's URL
+  generateRobotsTxt: true, // (Optional) Generates a robots.txt file
+};
