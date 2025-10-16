cf055cfd8c8504139ebb7c8d3d357eb7452b8a8c
Jamie Barlow <jamiebarlow@gmail.com>
2025-10-16 15:44:01 +0100

    Update layout for Contact page: centralised contact and address cards with responsive grid

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -48,77 +48,78 @@ const contact = async () => {
           size="200%"
           position="center"
         >
-          <div className="flex flex-2 flex-row flex-wrap container-padded section-middle gap-6 justify-center">
-            <div
-              id="contact-details"
-              className="flex flex-1 flex-col gap-10 text-base-content items-center md:items-start"
-            >
-              <h3 className="heading">{sections[0].header}</h3>
-              <div className="flex flex-row flex-wrap gap-6 justify-center sm:justify-start">
-                <IssueCard
-                  issue="phone"
-                  background="bg-base-100"
-                  title={phoneNumber}
-                  size="small"
-                  uniformSize
-                />
-                <IssueCard
-                  issue="email"
-                  background="bg-base-100"
-                  className="w-fit px-4"
-                  title={email}
-                  size="small"
-                  uniformSize
-                />
-              </div>
-
-              <div
-                id="addresses"
-                className="flex flex-row flex-wrap gap-6 w-full justify-center md:justify-start"
-              >
-                <div className="card w-3xs bg-base-100 shadow-lg">
-                  <figure className="relative w-full h-[251px]">
-                    <Map
-                      locations={[location1]}
-                      addresses={[location1Address]}
-                      className="w-full h-[400px] object-cover"
-                    />
-                  </figure>
-                  <div className="ps-0 py-0 flex flex-col md:flex-row flex-wrap flex-1 gap-6">
-                    <div className="relative w-full">
-                      <ContentfulImage asset={location1Image} quality={100} />
+          <div className="flex flex-row flex-wrap container-padded section-middle justify-center gap-10">
+            <section id="contact-details flex-1">
+              <h3 className="heading text-base-content text-center pb-6">
+                {sections[0].header}
+              </h3>
+              <div className="grid gap-10 text-base-content">
+                <div className="grid sm:grid-cols-2 gap-6 justify-items-center">
+                  <IssueCard
+                    issue="phone"
+                    background="bg-base-100"
+                    title={phoneNumber}
+                    size="small"
+                    uniformSize
+                  />
+                  <IssueCard
+                    issue="email"
+                    background="bg-base-100"
+                    title={email}
+                    size="small"
+                    uniformSize
+                  />
+                </div>
+                <div
+                  id="addresses"
+                  className="grid grid-cols-1 md:grid-cols-2 gap-6 justify-items-center text-base-content"
+                >
+                  <div className="card w-3xs bg-base-100 shadow-lg">
+                    <figure className="relative w-full h-[251px]">
+                      <Map
+                        locations={[location1]}
+                        addresses={[location1Address]}
+                        className="w-full h-[400px] object-cover"
+                      />
+                    </figure>
+                    <div className="ps-0 py-0 flex flex-col md:flex-row flex-wrap flex-1 gap-6">
+                      <div className="relative w-full">
+                        <ContentfulImage asset={location1Image} quality={100} />
+                      </div>
+                      <div className="flex-1 px-6 pb-6">
+                        <RichTextRenderer documents={location1Address} />
+                      </div>
                     </div>
-                    <div className="flex-1 px-6 pb-6">
-                      <RichTextRenderer documents={location1Address} />
+                  </div>
+                  <div className="card w-3xs bg-base-100 shadow-lg">
+                    <figure className="relative w-full h-[251px]">
+                      <Map
+                        locations={[location2]}
+                        addresses={[location2Address]}
+                        className="w-full h-[400px] object-cover"
+                      />
+                    </figure>
+                    <div className="ps-0 py-0 flex flex-col md:flex-row flex-wrap flex-1 gap-6">
+                      <div className="relative w-full">
+                        <ContentfulImage asset={location2Image} quality={100} />
+                      </div>
+                      <div className="flex-1 px-6 pb-6">
+                        <RichTextRenderer documents={location2Address} />
+                      </div>
                     </div>
                   </div>
                 </div>
-                <div className="card w-3xs bg-base-100 shadow-lg">
-                  <figure className="relative w-full h-[251px]">
-                    <Map
-                      locations={[location2]}
-                      addresses={[location2Address]}
-                      className="w-full h-[400px] object-cover"
-                    />
-                  </figure>
-                  <div className="ps-0 py-0 flex flex-col md:flex-row flex-wrap flex-1 gap-6">
-                    <div className="relative w-full">
-                      <ContentfulImage asset={location2Image} quality={100} />
-                    </div>
-                    <div className="flex-1 px-6 pb-6">
-                      <RichTextRenderer documents={location2Address} />
-                    </div>
-                  </div>
-                </div>
               </div>
-            </div>
-            <div
+            </section>
+            <section
               id="contact-form"
               className="flex flex-1 flex-col text-base-content"
             >
-              <h3 className="heading">Contact Form</h3>
+              <h3 className="heading text-base-content text-center pb-6">
+                Contact Form
+              </h3>
               <ContactForm />
-            </div>
+            </section>
           </div>
         </BackgroundTexture>
       </main>
