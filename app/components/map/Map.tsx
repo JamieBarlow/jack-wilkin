605e01b4e079d9b4769b809546533a604fac4726
Jamie Barlow <jamiebarlow@gmail.com>
2026-01-21 21:05:17 +0000

    Add popup to map marker for multiple locations. Controlled with state

--- app/components/map/Map.tsx
+++ app/components/map/Map.tsx
@@ -3,11 +3,7 @@ import RichTextRenderer from "../RichTextRenderer";
 import { Document, BLOCKS } from "@contentful/rich-text-types";
 import { Options } from "@contentful/rich-text-react-renderer";
 
-// import dynamic from "next/dynamic";
 import "mapbox-gl/dist/mapbox-gl.css";
-// const Mapbox = dynamic(() => import("react-map-gl/mapbox").then((m) => m.Map), {
-//   ssr: false,
-// });
 import { useState } from "react";
 import { Map as Mapbox, Marker, Popup } from "react-map-gl/mapbox";
 
@@ -25,8 +21,20 @@ interface Location {
   lon: number;
 }
 
+// Styling for RichTextRenderer popup
+const Text: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+  <p className="text-xs leading-0.5 text-base-content">{children}</p>
+);
+const options: Options = {
+  renderNode: {
+    [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
+  },
+  renderText: (text) => text.replace("!", "?"),
+};
+
 const Map = ({ locations, addresses }: MapProps) => {
-  const [showPopup, setShowPopup] = useState<boolean>(true);
+  // Controls which popup is displayed on clicking a marker
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
 
   // Get average (centre) lat and lon values from coordinate data
   let lat = locations[0].lat;
@@ -36,13 +44,15 @@ const Map = ({ locations, addresses }: MapProps) => {
     lon = locations.reduce((sum, loc) => sum + loc.lon, 0) / locations.length;
   }
 
+  const lat_offset = 0.01;
+
   return (
     <Mapbox
       key="mapbox-instance"
       mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
       initialViewState={{
         longitude: lon,
-        latitude: lat,
+        latitude: lat + lat_offset,
         zoom: 12,
       }}
       style={{ width: "100%", height: 400 }}
@@ -56,50 +66,34 @@ const Map = ({ locations, addresses }: MapProps) => {
             longitude={location.lon}
             anchor="bottom"
             color="rgba(232, 95, 92, 1)"
+            onClick={(e) => {
+              e.originalEvent.stopPropagation();
+              setActiveIndex(index);
+            }}
           ></Marker>
         );
       })}
-      {showPopup && (
+      {activeIndex !== null && (
         <Popup
-          offset={25}
-          longitude={locations[0].lon}
-          latitude={locations[0].lat}
+          offset={40}
+          longitude={locations[activeIndex].lon}
+          latitude={locations[activeIndex].lat}
           closeButton
+          onClose={() => {
+            setActiveIndex(null);
+          }}
         >
-          You are here
+          <div className="p-1">
+            <RichTextRenderer
+              documents={addresses[activeIndex]}
+              options={options}
+            />
+          </div>
         </Popup>
       )}
       ;
     </Mapbox>
   );
 };
-
-// {locations.map((location, index) => {
-//         return (
-//           <Marker
-//             position={[location.lat, location.lon]}
-//             // icon={MarkerIcon}
-//             key={index}
-//           >
-//             <Popup keepInView>
-//               <RichTextRenderer
-//                 documents={addresses[index]}
-//                 options={options}
-//               />
-//             </Popup>
-//           </Marker>
-//         );
-//       })}
-
-//   // Styling for RichTextRenderer
-//   const Text: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-//     <p className="text-xs leading-0.5">{children}</p>
-//   );
-//   const options: Options = {
-//     renderNode: {
-//       [BLOCKS.PARAGRAPH]: (node, children) => <Text>{children}</Text>,
-//     },
-//     renderText: (text) => text.replace("!", "?"),
-//   };
 
 export default Map;
